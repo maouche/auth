ed912e1c3fc5ba7bbc45f1f2dfd6bcbc106375d7
MAOUCHE Yacine <yacine92maouche@gmail.com>
2022-09-04 16:16:38 +0100

    load cors with diffrent env

--- config/server.js
+++ config/server.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const cors = require("cors");
+
+// .env
+dotenv.config({ path: '.env.local' });
+dotenv.config();
 
 // App init
 const app = express();
@@ -9,12 +14,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/assets", express.static("public"));
 
-app.use(
-    cors({
-        credentials: true,
-        origin: process.env.URL_UI,
-    })
-);
+// CORS
+if ( process.env.ENV === "dev") {
+    app.use(cors());
+} else {
+    app.use(
+        cors({
+            credentials: true,
+            origin: process.env.URL_UI,
+        })
+    );
+}
 
 app.listen(process.env.PORT, () =>
     console.log("listing to port " + process.env.PORT)
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require("dotenv").config();
 const app = require("./config/server");
 const routesAuth = require("./route/auth");
 
 // ADD ROUTES
 app.use("/api/v1", routesAuth);
+
+console.log(process.env.ACCESS_TOKEN_SECRET)
